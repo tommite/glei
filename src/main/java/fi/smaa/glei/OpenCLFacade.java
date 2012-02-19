96b066a5453e13fec89a0a077a3139d4ae87c335
Tommi Tervonen <tervonen@ese.eur.nl>
2012-02-19 19:17:10 +0100

    Add ocl facade template

---
+++ src/main/java/fi/smaa/glei/OpenCLFacade.java
@@ -0,0 +1,62 @@
+package fi.smaa.glei;
+
+import static org.jocl.CL.*;
+import org.jocl.CL;
+import org.jocl.cl_command_queue;
+import org.jocl.cl_context;
+import org.jocl.cl_context_properties;
+import org.jocl.cl_device_id;
+import org.jocl.cl_platform_id;
+
+public class OpenCLFacade {
+
+	private cl_context context;
+	private cl_command_queue commandQueue;
+
+	public OpenCLFacade() {
+		init();
+	}
+
+	public void init() {
+		// The platform, device type and device number
+		// that will be used
+		final int platformIndex = 0;
+		final long deviceType = CL_DEVICE_TYPE_ALL;
+		final int deviceIndex = 0;
+
+		// Enable exceptions and subsequently omit error checks in this sample
+		CL.setExceptionsEnabled(true);
+
+		// Obtain the number of platforms
+		int numPlatformsArray[] = new int[1];
+		clGetPlatformIDs(0, null, numPlatformsArray);
+		int numPlatforms = numPlatformsArray[0];
+
+		// Obtain a platform ID
+		cl_platform_id platforms[] = new cl_platform_id[numPlatforms];
+		clGetPlatformIDs(platforms.length, platforms, null);
+		cl_platform_id platform = platforms[platformIndex];
+
+		// Initialize the context properties
+		cl_context_properties contextProperties = new cl_context_properties();
+		contextProperties.addProperty(CL_CONTEXT_PLATFORM, platform);
+
+		// Obtain the number of devices for the platform
+		int numDevicesArray[] = new int[1];
+		clGetDeviceIDs(platform, deviceType, 0, null, numDevicesArray);
+		int numDevices = numDevicesArray[0];
+
+		// Obtain a device ID 
+		cl_device_id devices[] = new cl_device_id[numDevices];
+		clGetDeviceIDs(platform, deviceType, numDevices, devices, null);
+		cl_device_id device = devices[deviceIndex];
+
+		// Create a context for the selected device
+		context = clCreateContext(
+				contextProperties, 1, new cl_device_id[]{device}, 
+				null, null, null);
+
+		// Create a command-queue
+		commandQueue = clCreateCommandQueue(context, devices[0], 0, null);
+	}
+}
