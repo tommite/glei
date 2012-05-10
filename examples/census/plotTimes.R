f9dbffb2bcf5e0b4f4d4ed176fe53192599bab24
Tommi Tervonen <tervonen@ese.eur.nl>
2012-05-10 19:27:56 +0200

    Fix IV test script

--- examples/census/plotTimes.R
+++ examples/census/plotTimes.R
@@ -9,17 +9,13 @@ x=as.vector(dem.data$educ.IV)
 z=as.matrix(dem.data$qob.IV[,1:3])
 nr <- 1e6
 
-is.iv.tstudent(y, x, z)
-
 sizes <- ceiling(seq(1, 10) * length(x)/10)
 
 compRes <- function(gputype, nr, sizes) {
   t(sapply(sizes, function(len) {
-	print(len)
     t <- system.time(
                      res <- is.iv.tstudent(y=y[1:len], x=x[1:len], z=z[1:len,], nr=nr, useGPU=gputype>0, gpuType=gputype)
                      )[3]
-	Sys.sleep(2)
     return(c(t, res))
   }))
 }
@@ -28,6 +24,6 @@ resCPU <- compRes(0, nr, sizes)
 resGPU1 <- compRes(1, nr, sizes)
 
 pdf('runtimeplot-iv-gpu_vs_cpu.pdf')
-matplot(y=t(rbind(resCPU[,1], resGPU1[,1], resGPU2[,1])), pch=1, ylab='time(s)', x=sizes, xlab='size(data)')
-legend(x='topleft', legend=c('cpu', 'gpuv1', 'gpuv2'), pch=1, col=c(1,2,3))
+matplot(y=t(rbind(resCPU[,1], resGPU1[,1])), pch=1, ylab='time(s)', x=sizes, xlab='size(data)')
+legend(x='topleft', legend=c('cpu', 'gpu'), pch=1, col=c(1,2))
 dev.off()
